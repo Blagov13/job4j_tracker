6a4f2a096545082520ab7559a0b713baeeffdf08
aleksandrblagov <blagov13rus@gmail.com>
2023-11-08 11:29:36 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void sing() {
+        System.out.println("I believe I can fly");
+    }
+
+    public void accordion() {
+        System.out.println("Play Play Play");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.sing();
+        petya.sing();
+        petya.sing();
+        petya.accordion();
+        petya.accordion();
+        petya.accordion();
+    }
+}
