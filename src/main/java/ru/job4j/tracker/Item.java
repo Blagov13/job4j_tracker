952bd4c8dcc4c2fb5b1a5661552717e31e9cbf49
aleksandrblagov <blagov13rus@gmail.com>
2023-11-13 10:50:09 +0300

    8. Date. Отображение даты. [#310017 #459371]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -11,31 +11,32 @@ public class Item {
         return created;
     }
 
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.id = id;
-    }
-
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
+
+    public Item(String name) {
+        this.id = id;
+    }
+
+    public Item() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
