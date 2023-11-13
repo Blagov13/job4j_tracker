0572ad894a7fc1d7aa9f1d511ac931da5f67f5cd
aleksandrblagov <blagov13rus@gmail.com>
2023-11-13 16:25:42 +0300

    5. Tracker - хранилище [#396 #459505]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,8 +7,11 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
     }
 
     public Item(int id, String name) {
@@ -16,27 +19,24 @@ public class Item {
         this.name = name;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public Item(String name) {
-        this.id = id;
-    }
-
-    public Item() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
 
