0af04a21d12b091b51945f542350287abeaac48c
aleksandrblagov <blagov13rus@gmail.com>
2023-11-28 10:30:06 +0300

    0. Map, HashMap [#212652 #462850]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("blagov13rus@gmail.com", "Blagov Aleksandr Alekseevich");
+        map.put("blagova13@Gmail.com", "Blagova Daria Sergeevna");
+        map.put("blagov13rus@gmail.com", "Blagov Barni Aleksandrovich");
+        map.put("Student@Gmail.com", "Novikov Aleksandr Alekseeevich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
