21e07f3fdd1872c7ac5fa586bfffae4c547bbe43
aleksandrblagov <blagov13rus@gmail.com>
2023-11-29 12:27:13 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/collection/ItemSorterTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.ItemAscByName;
+import ru.job4j.tracker.ItemDescByName;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ItemSorterTest {
+    @Test
+    public void whenSortAscByName() {
+        List<Item> items = Arrays.asList(
+                new Item("Phone"),
+                new Item("Book"),
+                new Item("Milk"),
+                new Item("Alphabet")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item("Alphabet"),
+                new Item("Book"),
+                new Item("Milk"),
+                new Item("Phone")
+        );
+        items.sort(new ItemAscByName());
+        assertThat(items.toString()).isEqualTo(expected.toString());
+    }
+
+    @Test
+    public void whenSortDescByName() {
+        List<Item> items = Arrays.asList(
+                new Item("Phone"),
+                new Item("Book"),
+                new Item("Milk"),
+                new Item("Alphabet")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item("Phone"),
+                new Item("Milk"),
+                new Item("Book"),
+                new Item("Alphabet")
+        );
+        items.sort(new ItemDescByName());
+        assertThat(items.toString()).isEqualTo(expected.toString());
+    }
+}
