52a0dd4107c4c61b58e1f7dae7ba9d40aa651879
aleksandrblagov <blagov13rus@gmail.com>
2023-12-05 11:29:55 +0300

    3. Optional - empty, of [#214711 #464436]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -29,7 +29,6 @@ public class College {
                     return Optional.of(s);
                 }
             }
-            return Optional.empty();
         }
         return Optional.empty();
     }
