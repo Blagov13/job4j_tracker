75797ce8c2f7e60acc643fd8837b848448465b06
aleksandrblagov <blagov13rus@gmail.com>
2024-05-28 12:34:17 +0300

    2. @Setter, @Getter и другие [#504889]

---
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -0,0 +1,16 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class Category {
+    @NonNull
+    @EqualsAndHashCode.Include
+    @Getter
+    private int id;
+    @Getter
+    @Setter
+    private String name;
+}
